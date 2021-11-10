dcff8a9a6044c22c5adc7ba186d2006d6eb21e3e
Niraj <niraj@ashutec.com>
2021-11-10 18:16:50 +0530

    last-modify-Guess-my-number-code

--- starter/script.js
+++ starter/script.js
@@ -36,9 +36,19 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High' : 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
 
     //when number is high
-  } else if (guess > secretNumber) {
+  } /*else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
       score--;
@@ -58,7 +68,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lost the Game';
       document.querySelector('.score').textContent = 0;
     }
-  }
+  }*/
 });
 
 document.querySelector('.again').addEventListener('click', function () {
